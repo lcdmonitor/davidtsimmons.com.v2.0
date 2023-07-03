8d636a3f0c00f2fa3fb9ba5188997e58f451228e
Dave Simmons <dave@davidtsimmons.com>
2023-07-03 01:04:14 -0400

    added timestamp to logging

--- davidtsimmons.com/Program.cs
+++ davidtsimmons.com/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddLogging(opt=>
 {
     opt.AddSimpleConsole(c=>
     {
-        c.TimestampFormat = "[HH:mm:ss] ";
+        c.TimestampFormat = "yyyy-MM-dd [HH:mm:ss] ";
     });
 });
 #endregion
