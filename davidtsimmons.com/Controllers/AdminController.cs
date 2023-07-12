80e8c5299c09c97f55c0e6cc1871ed2cd481083d
Dave Simmons <dave@davidtsimmons.com>
2023-07-12 15:18:29 -0400

    added admin section

---
+++ davidtsimmons.com/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace davidtsimmons.com.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
